9c8d2d8ad7a78a906e1da57f26128d7267c215b6
Oleksandr <alexsstorchak@gmail.com>
2022-05-13 15:34:21 +0300

    devise views for all models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-                       sessions: "users/sessions",
-                       registrations: "users/registrations",
-                     }
+  devise_for :users
   devise_for :admins
+
   root "pages#index"
 end
