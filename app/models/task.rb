dbec5812d1b1fd71c584e38342d26364e0753486
Oleksandr <alexsstorchak@gmail.com>
2022-05-20 13:42:34 +0300

    add column status

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :user
 
+  enumerize :status,
+            in: { :todo => 1, :inprogress => 2, :done => 3 },
+            default: :todo,
+            predicates: true
+
   validates :title, presence: { message: "Title blank" },
                     length: { minimum: 5, message: "Title min 5 chars" }
   validates :description, presence: { message: "Description blank" },
