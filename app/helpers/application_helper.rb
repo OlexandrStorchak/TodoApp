43e3498097cea703a0d6bcc48f4a9308af0f92da
Oleksandr <alexsstorchak@gmail.com>
2022-05-21 21:13:05 +0300

    views edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,7 @@
 module ApplicationHelper
   include Pagy::Frontend
 
-  def loged_user
-    current_admin || current_user
-  end
-
   def form_errors_helper(obj, column)
     render partial: "layouts/errors", locals: { obj: obj, column: column } if obj.errors.where(column).any?
-  end
-
-  def sort_reverse_button
-    if params[:sort_status] == "desc"
-      link_to " Sort by status", tasks_path + "?sort_status=asc", class: "btn btn-sm btn-outline-info ms-1 bi bi-arrow-down-up"
-    else
-      link_to " Sort by status", tasks_path + "?sort_status=desc", class: "btn btn-sm btn-outline-info ms-1 bi bi-arrow-down-up"
-    end
   end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -7,4 +7,12 @@ module TasksHelper
   def index_page_tasks_list_empty(tasks)
     link_to "Create new task", new_task_path, class: "btn btn btn-outline-primary" if current_user.tasks.empty?
   end
+
+  def sort_reverse_button
+    if params[:sort_status] == "desc"
+      link_to " Sort by status", tasks_path + "?sort_status=asc", class: "btn btn-sm btn-outline-info ms-1 bi bi-arrow-down-up"
+    else
+      link_to " Sort by status", tasks_path + "?sort_status=desc", class: "btn btn-sm btn-outline-info ms-1 bi bi-arrow-down-up"
+    end
+  end
 end
