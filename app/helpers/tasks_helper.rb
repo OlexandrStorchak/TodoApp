216a638c517cf2ea9f2b033f869d7842f95c8d09
Oleksandr <alexsstorchak@gmail.com>
2022-05-21 13:26:37 +0300

    add empty tasks button

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -3,4 +3,8 @@ module TasksHelper
     time = task.created_at + 2.hours
     local_time(time, :time)
   end
+
+  def index_page_tasks_list_empty(tasks)
+    link_to "Create new task", new_task_path, class: "btn btn btn-outline-primary mt-50" if tasks.empty?
+  end
 end
