024f8c7bd4e34c1e96b7996e71f8a99eeb5329ac
Oleksandr <alexsstorchak@gmail.com>
2022-05-27 16:01:44 +0300

    refactor

--- spec/services/tasks_fetcher_spec.rb
+++ spec/services/tasks_fetcher_spec.rb
@@ -14,42 +14,36 @@ RSpec.describe Tasks::TasksFetchService do
   let(:filter_tasks_by_status) { Tasks::TasksFetchService.new(user, { status: 'todo' }).call }
 
   describe '#call' do
-    context '#all_tasks response hash' do
+    context 'all tasks response hash' do
       it { expect(tasks_blank_params).to include(:tasks) }
     end
-    context '#show_all_users_tasks_admin response hash with msg' do
+    context 'show all users tasks response hash' do
       it { expect(show_all_users_tasks_admin).to include(:tasks, :msg) }
     end
-  end
 
-  describe '#call' do
-    context '#all_users_tasks for admin' do
+    context 'all users tasks when admin' do
       it { expect(show_all_users_tasks_admin[:msg]).to eq('All users tasks') }
     end
-    context '#all_tasks_by_user for admin' do
+    context 'all tasks by user when admin' do
       it { expect(all_tasks_by_user_admin[:msg]).to eq("Tasks by user : #{user.email}") }
     end
-  end
 
-  describe '#call' do
-    context '#all_tasks for user' do
+    context 'all tasks when user' do
       it { expect(tasks_blank_params[:tasks].class).to be_a_kind_of(Task.class) }
     end
-    context '#all_users_tasks for user' do
+    context 'all users tasks when user' do
       it { expect(show_all_users_tasks[:msg]).not_to eq('All users tasks') }
     end
-    context '#all_tasks_by_user for user' do
+    context 'all tasks by user when user' do
       it { expect(all_tasks_by_user[:msg]).not_to eq("Tasks by user : #{user.email}") }
     end
-    context '#sort_tasks_by_status for user' do
+    context 'sort tasks by status when user' do
       it { expect(sort_tasks_by_status[:msg]).to eq('Sort by status') }
     end
-    context '#filter_tasks_by_status for user' do
+    context 'filter tasks by status when user' do
       it { expect(filter_tasks_by_status[:msg]).to eq('Filtered by "Todo" status') }
     end
-  end
 
-  describe '#call' do
     context 'compare results' do
       it { expect(tasks_blank_params[:tasks]).to eq(user_task) }
     end
