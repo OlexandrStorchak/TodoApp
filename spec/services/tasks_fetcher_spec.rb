8d115f8d6c750c0d42ec6c0b96ecefe8b707533d
Oleksandr <alexsstorchak@gmail.com>
2022-05-27 15:23:16 +0300

    try to refactor rspec

--- spec/services/tasks_fetcher_spec.rb
+++ spec/services/tasks_fetcher_spec.rb
@@ -1,45 +1,57 @@
 require 'rails_helper'
 RSpec.describe Tasks::TasksFetchService do
-  describe 'TasksFetchService' do
-    let(:user) { create :user, :user }
-    let(:admin) { create :user, :admin }
-    let(:user_task) { create_list :task, 10, user_id: user.id }
-    let(:admin_task) { create_list :task, 10, user_id: admin.id }
-    it '#all_tasks' do
-      result = Tasks::TasksFetchService.new(user, {}).call
-      expect(result[:tasks].class).to be_a_kind_of(Task.class)
+  let(:user) { create :user, :user }
+  let(:admin) { create :user, :admin }
+  let(:user_task) { create_list :task, 10, user_id: user.id }
+
+  let(:show_all_users_tasks_admin) { Tasks::TasksFetchService.new(admin, { all_users_tasks: 'show' }).call }
+  let(:all_tasks_by_user_admin) { Tasks::TasksFetchService.new(admin, { user_id: user.id }).call }
+
+  let(:tasks_blank_params) { Tasks::TasksFetchService.new(user, {}).call }
+  let(:show_all_users_tasks) { Tasks::TasksFetchService.new(user, { all_users_tasks: 'show' }).call }
+  let(:all_tasks_by_user) { Tasks::TasksFetchService.new(user, { user_id: user.id }).call }
+  let(:sort_tasks_by_status) { Tasks::TasksFetchService.new(user, { sort_status: 'desc' }).call }
+  let(:filter_tasks_by_status) { Tasks::TasksFetchService.new(user, { status: 'todo' }).call }
+
+  describe '#call' do
+    context '#all_tasks response hash' do
+      it { expect(tasks_blank_params).to include(:tasks) }
     end
-    it '#all_users_tasks for admin' do
-      result = Tasks::TasksFetchService.new(admin, { all_users_tasks: 'show' }).call
-      expect(result[:msg]).to eq('All users tasks')
+    context '#show_all_users_tasks_admin response hash with msg' do
+      it { expect(show_all_users_tasks_admin).to include(:tasks, :msg) }
     end
-    it '#all_users_tasks for non adnim' do
-      result = Tasks::TasksFetchService.new(user, { all_users_tasks: 'show' }).call
-      expect(result[:msg].class).not_to eq('All users tasks')
+  end
+
+  describe '#call' do
+    context '#all_users_tasks for admin' do
+      it { expect(show_all_users_tasks_admin[:msg]).to eq('All users tasks') }
     end
-    it '#all_tasks_by_user for admin' do
-      result = Tasks::TasksFetchService.new(admin, { user_id: user.id }).call
-      expect(result[:msg]).to eq("Tasks by user : #{user.email}")
+    context '#all_tasks_by_user for admin' do
+      it { expect(all_tasks_by_user_admin[:msg]).to eq("Tasks by user : #{user.email}") }
     end
-    it '#all_tasks_by_user for non admin' do
-      result = Tasks::TasksFetchService.new(user, { user_id: user.id }).call
-      expect(result[:msg]).not_to eq("Tasks by user : #{user.email}")
+  end
+
+  describe '#call' do
+    context '#all_tasks for user' do
+      it { expect(tasks_blank_params[:tasks].class).to be_a_kind_of(Task.class) }
     end
-    it '#sort_tasks_by_status' do
-      result = Tasks::TasksFetchService.new(user, { sort_status: 'desc' }).call
-      expect(result[:msg]).to eq('Sort by status')
+    context '#all_users_tasks for user' do
+      it { expect(show_all_users_tasks[:msg]).not_to eq('All users tasks') }
     end
-    it '#filter_tasks_by_status' do
-      result = Tasks::TasksFetchService.new(user, { status: 'todo' }).call
-      expect(result[:msg]).to eq('Filtered by "Todo" status')
+    context '#all_tasks_by_user for user' do
+      it { expect(all_tasks_by_user[:msg]).not_to eq("Tasks by user : #{user.email}") }
     end
-    it 'compare result tasks list with expected user' do
-      result = Tasks::TasksFetchService.new(user, {}).call
-      expect(result[:tasks]).to eq(user_task)
+    context '#sort_tasks_by_status for user' do
+      it { expect(sort_tasks_by_status[:msg]).to eq('Sort by status') }
     end
-    it 'compare result tasks list with expected admin' do
-      result = Tasks::TasksFetchService.new(admin, {}).call
-      expect(result[:tasks]).to eq(admin_task)
+    context '#filter_tasks_by_status for user' do
+      it { expect(filter_tasks_by_status[:msg]).to eq('Filtered by "Todo" status') }
+    end
+  end
+
+  describe '#call' do
+    context 'compare results' do
+      it { expect(tasks_blank_params[:tasks]).to eq(user_task) }
     end
   end
 end
